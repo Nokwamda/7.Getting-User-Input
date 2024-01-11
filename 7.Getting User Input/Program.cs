8ebcb4df98108800d9737c8ccdec5f44a9ce6569
Nokwanda Khubisa <Nokwanda.Khubisa@standardbank.co.za>
2024-01-11 07:28:06 +0200

    Add project files.

---
+++ 7.Getting User Input/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Getting_User_Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //use write not weriteline
+            Console.Write("Enter your name ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi " + name);
+            Console.Write("How old are you ");
+            //The values from the user are always a string
+            string age = Console.ReadLine();
+            Console.WriteLine("You are " + age + " of age");
+
+            Console.ReadLine();
+
+        }
+    }
+}
